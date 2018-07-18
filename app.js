f1ec74b4a63a43ca249a9fffc7037369a1ec439e
CPBCPB <christinebeck2010@gmail.com>
2018-07-18 17:14:29 -0400

    made env changes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const Meet        =require('./models/meet');
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('process.env.MONGODB_URI', {useMongoClient: true})
+  .connect(process.env.MONGODB_URI, {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
