e84e501d1292ad5a0be462cb4888e5b9adccb2b7
CPBCPB <christinebeck2010@gmail.com>
2018-07-19 17:55:46 -0400

    fixed thingy

--- models/meet.js
+++ models/meet.js
@@ -1,21 +1,14 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
-
 const meetSchema = new Schema({
-//the user who created the appointment
     business: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-//the user who reserved the appointment
     client: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-//start date and time of upcoming meeting
     datetime: {type: Date, required: true},
-//duration of upcoming meeting
     durationinMinutes: {type: Number, default: 30, enum:[30, 45, 60, 90, 120]},
     status: {type: String, default: 'upcoming', enum:['upcoming', 'canceled', 'missed', 'completed',]},
   });
 
-
   const Meet = mongoose.model("Meet", meetSchema);
-
 
   module.exports = Meet;
